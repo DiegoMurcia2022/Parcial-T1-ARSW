4052d38831a1e81b95bfd7442e012d9c9996fa59
Diego Alejandro Murcia Cespedes <diegomurciac2220@gmail.com>
2021-02-24 11:04:04 -0500

    API tested and finished

--- covidAnalyzerApi/src/main/java/eci/arsw/covidanalyzer/service/ICovidAggregateServiceStub.java
+++ covidAnalyzerApi/src/main/java/eci/arsw/covidanalyzer/service/ICovidAggregateServiceStub.java
@@ -2,25 +2,61 @@ package eci.arsw.covidanalyzer.service;
 
 import eci.arsw.covidanalyzer.model.Result;
 import eci.arsw.covidanalyzer.model.ResultType;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.CopyOnWriteArrayList;
 
-@Service("ICovidAggregateServiceStub")
+@Component("ICovidAggregateServiceStub")
 public class ICovidAggregateServiceStub implements ICovidAggregateService {
+    private List<Result> resultList = new CopyOnWriteArrayList<>();
+
     @Override
-    public boolean aggregateResult(Result result, ResultType type) {
-        return false;
+    public void aggregateResult(Result result, ResultType type) {
+        int testNumber;
+        for (Result r:resultList) {
+            if (r.equals(result)) {
+                r.setResultType(type);
+
+                testNumber = r.getTestRealize();
+                testNumber += 1;
+                r.setTestRealize(testNumber);
+            }
+        }
     }
 
     @Override
-    public boolean getResult(ResultType type) {
-        return false;
+    public List<Result> getResult(ResultType type) {
+        int testNumber;
+        List<Result> resultsByType = new CopyOnWriteArrayList<>();
+
+        for (Result r:resultList) {
+            if (r.getResultType().equals(type)) {
+                resultsByType.add(r);
+
+                testNumber = r.getTestRealize();
+                testNumber += 1;
+                r.setTestRealize(testNumber);
+            }
+        }
+
+        return resultsByType;
     }
 
 
     @Override
     public void upsertPersonWithMultipleTests(UUID id, ResultType type) {
+        int testNumber;
 
+        for (Result r:resultList) {
+            if (r.getId().equals(id)) {
+                r.setResultType(type);
+
+                testNumber = r.getTestRealize();
+                testNumber += 1;
+                r.setTestRealize(testNumber);
+            }
+        }
     }
 }
